50358a5c524f9511afff7cdd5f9f5c5161550528
Jasper Ayawan <ejayawan22@gmail.com>
2024-05-31 18:56:48 +0800

    update Auth buttons

--- src/components/Nav/AuthCTA.jsx
+++ src/components/Nav/AuthCTA.jsx
@@ -4,8 +4,8 @@ const AuthCTA = () => {
     
   return (
     <div className='flex flex-row gap-x-2'>
-      <button className='btn'>Log in</button>
-      <button className='btn bg-[#11112F] text-white dark:bg-yellow-bg dark:text-primary-color'>Create account</button>
+      <button className='btn btn-md'>Log in</button>
+      <button className='btn btn-md bg-[#11112F] text-white dark:bg-yellow-bg dark:text-primary-color'>Create account</button>
     </div>
   )
 }
