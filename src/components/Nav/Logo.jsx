314d8c0d2917521c2ed42673939295b2155d95d8
ejayawan22 <ejayawan22@gmail.com>
2024-06-10 19:26:56 +0800

    update Logo on the darkmode side

--- src/components/Nav/Logo.jsx
+++ src/components/Nav/Logo.jsx
@@ -1,10 +1,22 @@
 import React from 'react'
 import DivFadeRight from '../../animations/DivFadeRight'
+import useDarkModeStore from '../../store/useDarkModeStore';
 
-const Logo = () => {
+const Logo = ({ isScrolled }) => {
+  const { isDarkMode } = useDarkModeStore();
+
   return (
     <DivFadeRight>
-      <img src="/logo.svg" alt="" className='object-cover'/>
+      {isDarkMode ? (
+        <>
+       {isScrolled ? <span className='text-white font-Montserrat text-4xl font-semibold'>P<span className='text-primary-color'>E</span>CADO</span> : <span className='text-white font-Montserrat text-4xl font-semibold'>P<span className='text-yellow-color'>E</span>CADO</span>}
+        </>
+      ) : (
+        <>
+        {isScrolled ? <span className='text-white font-Montserrat text-4xl font-semibold'>P<span className='text-yellow-color'>E</span>CADO</span> : <span className='text-primary-color font-Montserrat text-4xl font-semibold'>P<span className='text-yellow-color'>E</span>CADO</span>}
+        </>
+      )}
+      
     </DivFadeRight>
   )
 }
--- src/components/Nav/Nav.jsx
+++ src/components/Nav/Nav.jsx
@@ -27,7 +27,7 @@ const Nav = () => {
   return (
     <div ref={navRef} className={`${isScrolled ? 'bg-primary-bg dark:bg-yellow-bg duration-500' : ''} fixed top-0 left-0 w-full py-5 z-50 px-20`}>
       <div className="mx-auto max-w-screen-monitor flex w-full justify-between items-center">
-        <Logo />
+        <Logo isScrolled={isScrolled}/>
         <DivFadeLeft>
             <div className="flex flex-row justify-center items-center gap-x-2">
                 <AuthCTA setIsScrolled={setIsScrolled}/>
